c4a06ed8c335050a99957e2278672c677e9f6697
mimi843187 <mimi84317@gmail.com>
2014-03-10 23:49:09 +0800

    增加2
    增加constructor
    砍掉super
    將println做修改

--- src/AbstractDrawFunction.java
+++ src/AbstractDrawFunction.java
@@ -5,22 +5,23 @@ public abstract class AbstractDrawFunction {
 	
 	Scanner input = new Scanner(System.in);
 	double x = input.nextDouble();
-	System.out.println("Please enter the value of x : \n" + x);
 	
+	
+	System.out.println("Please enter the value of x : \n" + x);
 	x_2 x1 = new x_2();
 	System.out.println(x1.toString() + "\n");
 	sin_x x2 = new sin_x();
-	System.out.println("f(x) = sin(x) = \n" + );
+	System.out.println(x2.toString() + "\n");
 	cos_x x3 = new cos_x();
-	System.out.println("f(x) = cos(x) = \n" + );
+	System.out.println(x3.toString() + "\n");
 	tan_x x4 = new tan_x();
-	System.out.println("f(x) = tan(x) = \n" + );
+	System.out.println(x4.toString() + "\n");
 	cos_5sin_x x5 = new cos_5sin_x();
-	System.out.println("f(x) = cos(x) + 5sin(x) = \n" + );
+	System.out.println(x5.toString() + "\n");
 	cos_sin_x x6 = new cos_sin_x();
-	System.out.println("f(x) = 5cos(x) + sin(x) = \n" + );
+	System.out.println(x6.toString() + "\n" );
 	log_x x7 = new log_x();
-	System.out.println("f(x) = log(x) + x^2 = \n" + );
+	System.out.println(x7.toString() + "\n");
 	
 	}
 
@@ -29,36 +30,65 @@ class x_2 extends AbstractDrawFunction{
 	
 	
 	public x_2(){
-	super(x);
 	}
 	double end_x = Math.pow( x , 2 );
 	public String toString(){
-	return "f(x) = x^2 = " + x_2();
+	return "f(x) = x^2 = " + end_x;
 	}
 	
 }
 class sin_x extends AbstractDrawFunction{
-	super(x);
+	
+	public sin_x(){
+	}
 	double end_sin = Math.sin(x);
+	public String toString(){
+	return "f(x) = sin(x) =" + end_sin;
+	}
 }
 
 class cos_x extends AbstractDrawFunction{
-	super(x);
 	
+	public cos_x(){
+	}
+	double end_cos = Math.cos(x);
+	public String toString(){
+		return "f(x) = cos(x) = " + end_cos;
+		}
 }
 class tan_x extends AbstractDrawFunction{
-	super(x);
 	
+	public tan_x(){
+	}
+	double end_tan = Math.tan(x);
+	public String toString(){
+		return "f(x) = tan(x) = " + end_tan;
+		}
 }
 class cos_5sin_x extends AbstractDrawFunction{
-	super(x);
 	
+	public cos_5sin_x(){
+	}
+	double end_cos_5sin = Math.cos(x) + 5 * Math.sin(x);
+	public String toString(){
+		return "f(x) = cos(x) + 5sin(x) = " +end_cos_5sin;
+		}
 }
 class cos_sin_x extends AbstractDrawFunction{
-	super(x);
 	
+	public cos_sin_x(){
+	}
+	double end_cos_sin = 5 * Math.cos(x) + Math.sin(x);
+	public String toString(){
+		return "f(x) = 5cos(x) + sin(x) = " + end_cos_sin;
+		}
 }
 class log_x extends AbstractDrawFunction{
-	super(x);
 	
+	public log_x(){
+	}
+	double end_log = Math.log(x);
+	public String toString(){
+		return "f(x) = log(x) + x^2 = " + end_log;
+		}
 }
